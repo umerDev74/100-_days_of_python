a7907e72e09a5ad76a150121c9956bdb8e253b6c
umerDev74 <umern7429@gmail.com>
2026-08-02 23:26:10 +0500

    3 new files

--- enumerate_function.py
+++ enumerate_function.py
@@ -3,3 +3,6 @@ for index, fruit in enumerate(fruits, start=1):
     print(index,fruit)
     if(index==4):
         print("Grapes are sour")
+
+
+#  in the loop start=1  shows that the index start from 1 -------
---
+++ short_hand_if-else.py
@@ -0,0 +1,5 @@
+a=700
+b=500
+print("A") if a>b else print("=") if a==b else print("B")
+c=23 if a>b else 0
+print(c)
