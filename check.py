360a57944ae1e868c31a7dc96056d5e218415a0f
umerDev74 <umern7429@gmail.com>
2026-08-10 14:38:07 +0500

    Added python import files and gitignore

---
+++ check.py
@@ -0,0 +1,9 @@
+def welcome():
+    print("hey you are welcome my friend")
+
+# Ye block sirf tab chalega jab aap DIRECT check.py ko chalayein ge.
+# Jab koi is file ko IMPORT karega, toh ye block NAHI chalega.
+
+if __name__ == "__main__":
+    print("Direct running check.py:")
+    welcome()
---
+++ how_import_work.py
@@ -0,0 +1,23 @@
+# import pandas as pd
+# pd.read_csv()
+
+# ----------------------------
+
+# import math
+# print(dir(math))
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# import check
+
+# check.welcome()
+
+# -------------- IF__NAME__ == "__main__" -----------------
+
+
+import check
+
+print("Main file ka __name__ hai:\n", __name__)
+
+# Yahan hum check.py ke welcome function ko call kar rahe hain
+check.welcome()
